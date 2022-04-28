163b6838c79c9c51fa7c738b41e3e8e5d3fa166c
Simcha Huchard <simchahuchard@hotmail.com>
2022-04-28 13:36:01 +0200

    some music volgens mij

---
+++ SynthViking/Assets/HomingProjectile.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoreMountains.Feedbacks; 
+
+public class HomingProjectile : MonoBehaviour
+{
+
+    public float aliveDuration;
+    public float aliveTimer;
+    public float hasTouchedTarget;
+    public float moveSpeed;
+    public Transform target; 
+    public 
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
